28e5408f12f81e7c7b8a0224dcedcb5b0271c179
Xingzi-Jackie-Zhou <xingzizhou@hotmail.com>
2024-08-29 17:30:47 -0600

    update text input for general information

--- controllers/profile-site-controller.js
+++ controllers/profile-site-controller.js
@@ -22,6 +22,53 @@ const allSites = async (req, res) => {
   }
 };
 
+const getSiteDetails = async (req, res) => {
+  try {
+    const { siteId } = req.params;
+    const userNameId = req.user?.username;
+    if (!userNameId) {
+      return res.status(400).json({ error: "User not authenticated" });
+    }
+    const user = await knex("users").where({ username: userNameId }).first();
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    const userId = user.id;
+    const site = await knex("main_record")
+      .where({ site_id: siteId, user_id: userId })
+      .first();
+    if (!site) {
+      return res.status(404).json({ error: "Site not found" });
+    }
+    const weatherID = site.city_id;
+    const city = await knex("weather_main_record")
+      .where({ weather_id: weatherID, user_id: userId })
+      .first();
+    if (!city) {
+      return res.status(404).json({ error: "Weather site not found" });
+    }
+
+    const dischargeData = await knex("discharge").where({
+      station_id: siteId,
+      user_id: userId,
+    });
+    const weatherData = await knex("weather").where({
+      climate_id: site.city_id,
+      user_id: userId,
+    });
+
+    res.status(200).json({
+      site,
+      discharge: dischargeData,
+      weather: weatherData,
+    });
+  } catch (error) {
+    console.error(`Error retrieving site details: ${error}`);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
 const deleteSite = async (req, res) => {
   try {
     const { siteId } = req.params;
@@ -66,30 +113,37 @@ const deleteSite = async (req, res) => {
   }
 };
 
-// const updateSite = async (req, res) => {
-//   try {
-//     const { siteId } = req.params;
-//     const { siteName, riverName, city, climateId } = req.body;
-//     const userId = req.user.id;
+const updateSite = async (req, res) => {
+  try {
+    const { siteId } = req.params;
+    const { siteName, riverName, city, climateId } = req.body;
+    const userId = req.user.id;
 
-//     const result = await knex("main_record")
-//       .where({ site_id: siteId, user_id: userId })
-//       .update({
-//         site_name: siteName,
-//         river: riverName,
-//         city_id: climateId,
-//         city: city
-//       });
+    const resultMain = await knex("main_record")
+      .where({ site_id: siteId, user_id: userId })
+      .update({
+        site_name: siteName,
+        site_id: siteId,
+        river: riverName,
+        city_id: climateId,
+      });
+    const weatherId = climateId;
+    const resultWeather = await knex("weather_main_record")
+      .where({ weather_id: weatherId, user_id: userId })
+      .update({
+        weather_site: city,
+        weather_id: climateId,
+      });
 
-//     if (result) {
-//       res.status(200).json({ message: "Site updated successfully" });
-//     } else {
-//       res.status(404).json({ error: "Site not found or not authorized" });
-//     }
-//   } catch (error) {
-//     console.error(`Error updating site: ${error}`);
-//     res.status(500).json({ error: "Internal Server Error" });
-//   }
-// };
+    if (result) {
+      res.status(200).json({ message: "Site updated successfully" });
+    } else {
+      res.status(404).json({ error: "Site not found or not authorized" });
+    }
+  } catch (error) {
+    console.error(`Error updating site: ${error}`);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
 
-export { allSites, deleteSite };
+export { allSites, deleteSite, getSiteDetails, updateSite };
--- routes/profile-user.js
+++ routes/profile-user.js
@@ -7,7 +7,11 @@ router.route("/sites").get(authenticateToken, profileSiteController.allSites);
 
 router
   .route("/sites/:siteId")
+  .get(authenticateToken, profileSiteController.getSiteDetails)
   .delete(authenticateToken, profileSiteController.deleteSite);
+
+// router
+//   .route("/sites/:siteId/edit")
 //   .put(authenticateToken, profileSiteController.updateSite);
 
 export default router;
